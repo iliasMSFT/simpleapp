87a937ab405cd9c1ef0f8cf7093c72bb931d1e5e
Ilias Jennane <iliasj@microsoft.com>
2019-12-17 12:15:25 -0500

    Added scripts for running all and cleaning all

---
+++ localDevTest/k8s/runAllMicroservices.ps1
@@ -0,0 +1,8 @@
+Write-Host "starting all simpleapp microservices" -ForegroundColor White
+kubectl apply -f simpleweb-configmap.yaml
+kubectl apply -f simpleweb-deployment.yaml
+kubectl apply -f simpleweb-loadbalancer.yaml
+kubectl apply -f simpleapi-deployment.yaml
+kubectl apply -f simpleapi-cluster.yaml
+Write-Host "The list of k8s objects running locally is:" -ForegroundColor Yellow
+kubectl get all
